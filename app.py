6821e18b529cfe9283a7c6d5e1922f579833c4a8
ValJhonNFT <101614628+ValJhonNFT@users.noreply.github.com>
2024-01-04 07:50:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 import os
+import threading
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+def test():
+    print("Test")
+ 
+update_thread = threading.Thread(target=test)
+update_thread.start()
