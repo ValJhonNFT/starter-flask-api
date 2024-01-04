6a62850ef80f4c5005c04deb8da1662aaa2d679b
ValJhonNFT <101614628+ValJhonNFT@users.noreply.github.com>
2024-01-04 08:01:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,30 @@ import os
 import threading
 import time
 
+global test
+
 app = Flask(__name__)
+
+# Global variable to track the thread status
+thread_running = False
+
+def test():
+    global thread_running
+    print("Test")
+    while True:
+        time.sleep(5)
+        print("Thread is running...")
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    global thread_running
+    if update_thread.is_alive():
+        thread_running = True
+    else:
+        thread_running = False
+    return f'Thread is running: {thread_running}'
 
-def test():
-    print("Test")
-    time.sleep(15)
- 
+
 update_thread = threading.Thread(target=test)
 update_thread.start()
